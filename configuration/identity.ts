e48a28fb4593789de492140dcb61dd0ea52e6229
David Phippen <parkerphippen@gmail.com>
2025-02-19 14:41:09 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Parker Phippen`;
 export const OWNER_DESCRIPTION: string = `a 1st year MBA candidate at UNC Kenan-Flagler`;
 
-export const AI_NAME: string = `Lance Armstrong`;
+export const AI_NAME: string = `Lance`;
 export const AI_TONE: string = `knowledgeable`;
 export const AI_ROLE: string = `road cycling expert`;
