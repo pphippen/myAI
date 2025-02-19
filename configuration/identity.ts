b7742a6dfb5d498d2a0355d3528585a0d4edfbb1
David Phippen <parkerphippen@gmail.com>
2025-02-19 14:10:43 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Parker Phippen`;
+export const OWNER_DESCRIPTION: string = `a 1st year MBA candidate at UNC Kenan-Flagler`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
